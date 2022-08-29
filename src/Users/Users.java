8f23fa5ba2e7152770b149a417c73fdd4d51b83d
Lucos18 <lucabuonpane3@gmail.com>
2022-08-29 12:22:55 +0200

    test

--- src/Users/Users.java
+++ src/Users/Users.java
@@ -31,14 +31,9 @@ public class Users {
     String sex;
     String email;
     String password;
-<<<<<<< HEAD
-    String CF;
-=======
-
     String CF = null;
->>>>>>> Test-Luca-Raffaele
     boolean role = false;
-    public Users(String name, String surname, String dateB, String cityB, String sex, String email, String password, String CF)
+    public Users(String name, String surname, String dateB, String cityB, String sex, String email, String password)
     {
         this.name = name;
         this.surname = surname;
@@ -47,20 +42,11 @@ public class Users {
         this.sex = sex;
         this.email = email;
         this.password = password;
-        this.CF=CF;
     }
-<<<<<<< HEAD
-    public Users(String name, String surname, String email, String password, String CF, Boolean role)
-=======
     public Users(String email, String password)
->>>>>>> Test-Luca-Raffaele
     {
         this.email = email;
         this.password = password;
-<<<<<<< HEAD
-        this.CF=CF;
-        this.role = role;
-=======
     }
     //Function to register a user in the University JSON, with different user information.
     public static boolean RegisterUser(Users user) throws IOException, InterruptedException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
@@ -291,7 +277,6 @@ public class Users {
     }
     public void setSex(String sex) {
         this.sex = sex;
->>>>>>> Test-Luca-Raffaele
     }
 }
 
